129c94a2861aca22d6ec8d0f52a124da6a9a66d7
Mehrdad <m_javidi@outlook.com>
2019-06-11 21:16:07 +0430

    add config, add readme.md

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  port: 8080,
+  host: '0.0.0.0'
+}
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import marketModel from './routes/marketModel'
-const bodyParser = require('body-parser')
-export const app = require('express')()
+import bodyParser from 'body-parser'
+import express from 'express'
+import config from './config'
+export const app = express()
 
 // configuration
 app.use(bodyParser.json())
 
 // routes
 app.post('/model/market', marketModel)
+
+app.listen(config.port, config.host, () => {
+  console.log(`Server listening on http://${config.host}:${config.port}`)
+})
